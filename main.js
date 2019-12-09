21a949eb6e32ae7bc8e465f4d8393cd71a49b8e8
phenix1229 <paul.garay@codeimmersives.com>
2019-12-09 13:51:54 -0500

    more done

--- main.js
+++ main.js
@@ -8,21 +8,38 @@ const Node = (value) => {
 const Lil = () => {
   return {
     head: null,
+    length: 0,
 
     values: function() {
-      const values = [];
-      for (const item of Lil){
-        values.push(item.value)
-      }
-      return values
+      // let newArr = [];
+      // for (const item of this){
+      //   newArr.push(item.value)
+      // }
+      // return values;
     },
 
     addToStart: function(value) {
-
+      const newNode = Node(value);
+      if (this.head === null){
+        this.head = newNode;
+      } else if (this.head !== null){
+        const oldHead = this.head;
+        newNode.next = oldHead;
+        this.head = newNode;
+      }
+      this.length++;
+      return this;
     },
 
     addToEnd: function(value) {
-    
+      const newNode = Node(value)
+      if (this.head === null){
+        this.head = newNode;
+      } else if (this.head !== null){
+        this.head.next = newNode;
+      }
+        this.length++;
+      return this;
     },
 
     removeFromStart: function() {
